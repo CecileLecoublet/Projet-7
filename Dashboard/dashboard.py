28cc0f43a2b01ca49099a48a40bee8d63344a1d7
CecileLecoublet <lecoublet.cecile@gmail.com>
2022-09-14 14:58:58 +0200

    Modification dashboard

--- Dashboard/dashboard.py
+++ Dashboard/dashboard.py
@@ -52,7 +52,7 @@ def main(val, df):
     headers = {
         'Content-Type' : 'application/json'
     }
-    url = "http://127.0.0.1:8000/predict?data="+payload
+    url = "https://p8credit.herokuapp.com/predict?data="+payload
 
     response = requests.post(url)
 
